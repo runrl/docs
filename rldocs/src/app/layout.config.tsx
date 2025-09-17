1eeb686b88330a01aae2f7a920846e3ad910b74b
kaylanhua <huangkayla8@gmail.com>
2025-09-17 08:15:19 -0700

    better doxx

--- rldocs/src/app/layout.config.tsx
+++ rldocs/src/app/layout.config.tsx
@@ -4,8 +4,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Shared layout configurations
  *
  * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
+ * Main Layout: app/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
